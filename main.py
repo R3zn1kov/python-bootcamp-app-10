d9c66d16e8c924d6621cd39a1095a5eb53d44fa9
R3zn1kov <ldlreznikov@gmail.com>
2024-02-28 22:50:51 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import selectorlib
+from send_email import send_email
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(URL):
+    """"Scrape the page source from URL"""
+    response = requests.get(URL, HEADERS)
+    source = response.text
+    return source
+
+
+# displaytimer
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+
+    content = read(extracted)
+    if extracted != "No Upcoming Tours":
+        if extracted not in content:
+            store(extracted)
+            send_email(message="Yo, niente concerti")
